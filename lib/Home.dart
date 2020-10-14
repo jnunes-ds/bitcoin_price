c30d1acdbc4d40a96abb27a4044cdc75986d3a21
Júnior Nunes <denisonprobable13@gmail.com>
2020-10-13 21:24:03 -0300

    Modifications made to insert another button

--- lib/Home.dart
+++ lib/Home.dart
@@ -30,34 +30,16 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: Container(
         padding: EdgeInsets.all(32),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset("images/bitcoin.png"),
-              Padding(
-                padding: EdgeInsets.only(top: 30),
-                child: Text(
-                  "R\$" + _price,
-                  style: TextStyle(
-                    fontSize: 35
-                  )
-                )
+        child: Column(
+          children: [
+            Image.asset("images/bitcoin.png"),
+            Text(
+              "value",
+              style: TextStyle(
+                fontSize: 60
               ),
-              RaisedButton(
-                child: Text(
-                    "Check Again",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white
-                  ),
-                ),
-                color: Colors.orange,
-                padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
-                onPressed: _recoverPrice,
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -7,3 +7,4 @@ void main() => runApp(
     debugShowCheckedModeBanner: false,
   )
 );
+//ok
